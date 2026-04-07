d171c0a8a6df55db017a1300a1ada472c7bc1d11
A.Robot <SamsonClaw@proton.me>
2026-04-06 23:19:59 -0400

    Scrub internal workflow files and agent skills from public repository

--- check_tx.py
+++
@@ -1,9 +0,0 @@
-import json
-with open('blockchain.json', 'r') as f:
-    data = json.load(f)
-
-for b in data:
-    if len(b['transactions']) > 1:
-        print(f"Block {b['index']} has {len(b['transactions'])} txs!")
-        for tx in b['transactions']:
-            print(tx['tx_id'], tx['outputs'])
